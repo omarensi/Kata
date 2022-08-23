8acbd49b8fe48b204603a68c5d854f235cd12010
Omar Abdelli <omar.abdelli@ratp.fr>
2022-08-23 14:21:54 +0200

    kata first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Kata
-This app allows user to manage his wallet
+This app allows user to manage his wallet.
+
+This app need to be tested. We have not implemented unit tests because we can't use framework in this project.
+
+A dockerfile is added.
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,32 +17,13 @@ public class Main {
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 			
 			Client client = new Client("12345678", "102030", "Eden", "Dupont", "edin.dupont@gmail.com", "81DC9BDB52D04DC20036DBD8313ED055");
-		    //le password de 4 chiffre à introduire est: 1234 
+		    //le password de 4 chiffre ï¿½ introduire est: 1234 
 			Account account = new Account("0054321", client);
 		    List<Transaction> transactions = new ArrayList();
 
 		    Monitor monitor = new Monitor();
 		    monitor.displayMenu(client, account, transactions);
-		   
-		    
-//		    String hash = "35454B055CC325EA1AF2126E27707052";
-//		    String password = "1234";
-//		        
-//		    MessageDigest md = MessageDigest.getInstance("MD5");
-//		    md.update(password.getBytes());
-//		    byte[] digest = md.digest();
-//		    String myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
-//		    
-//		    String password1 = "1234";
-//	        
-//		    MessageDigest md1 = MessageDigest.getInstance("MD5");
-//		    md1.update(password1.getBytes());
-//		    byte[] digest1 = md1.digest();
-//		    String myHash1 = DatatypeConverter.printHexBinary(digest1).toUpperCase();
-//		    
-//		    
-//		    System.out.println(myHash);
-//		    System.out.println(myHash1);
+
 	}
 
 }
